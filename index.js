1a29ac1af1f7e6b631bc59b6af5835639bd5987e
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-06-29 21:13:05 -0400

    refactor: Add game cards to page dynamically

--- index.js
+++ index.js
@@ -29,26 +29,33 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for (let i = 0; i < games.length; i++) {
 
         // create a new div element, which will become the game card
-
+        const game = document.createElement("div");
 
         // add the class game-card to the list
+        game.classList.add("game-card");
 
-
-        // set the inner HTML using a template literal to display some info 
+        // set the inner HTML using a template literal to display some info
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
+        game.innerHTML = `
+            <img class="game-img" src="${games[i].img}" alt="${games[i].name}"/>
+            <h2>${games[i].name}</h2>
+            <p>Players: ${games[i].players}</p>`;
 
         // append the game to the games-container
+        gamesContainer.appendChild(game);
 
+    }
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
+addGamesToPage(GAMES_JSON);
+
 
 
 /*************************************************************************************
